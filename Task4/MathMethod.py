593455dc04e6351878a614600e6bd2d5b5da988b
Impervguin <leon53000@yandex.ru>
2024-04-25 01:14:59 +0300

    Done 2nd task

--- Task4/MathMethod.py
+++ Task4/MathMethod.py
@@ -15,6 +15,7 @@ def LeastSquare(x, y, p, n, xp):
     return sum([a[i] * xp ** i for i in range(n + 1)])
 
 def Gauss(CoeffMat, Rights):
+    # print(*CoeffMat, sep="\n")
     n = len(CoeffMat)
     if n != len(Rights):
         raise ValueError("Mat and Rights must have the same length")
@@ -35,7 +36,35 @@ def Gauss(CoeffMat, Rights):
     res = [Rights[i] / CoeffMat[i][i] for i in range(n)]
     return res
 
-x, y, p = read_xypdata("data/tmp41")
 
 
-print(LeastSquaresCoefs(x, y, p, 2))
+def LeastSquaresCoeffs2D(x, y, z, p, n):
+    dataLen = len(x)
+    if dataLen != len(y) or dataLen != len(p) or dataLen != len(p):
+        raise ValueError("x, y, z and p must have the same length")
+    
+    def aSum(xdeg, ydeg):
+        return sum([xn ** xdeg * yn ** ydeg * pn for xn, yn, zn, pn in zip(x, y, z, p)])
+    
+    def zSum(xdeg, ydeg):
+        return sum([xn ** xdeg * yn ** ydeg * zn * pn for xn, yn, zn, pn in zip(x, y, z, p)])
+    
+    aCoeffs = [[aSum(i + k, j + t) for k in range(n + 1) for t in range(n + 1 - k) ] for i in range(n + 1) for j in range(n + 1 - i) ]
+    zCoeffs = [zSum(i, j) for i in range(n + 1) for j in range(n + 1 - i) ]
+
+    return Gauss(aCoeffs, zCoeffs)
+
+def LeastSquare2D(x, y, z, p, n):
+    a = LeastSquaresCoeffs2D(x, y, z, p, n)
+    def func(xp, yp):
+        res = 0
+        ind = 0
+        for i in range(n + 1):
+            for j in range(n + 1 - i):
+                res += a[ind] * xp ** i * yp ** j
+                ind += 1
+        return res
+    return func
+
+# x, y, z, p = read_xypzdata("./data/tmp43")
+# print(LeastSquaresCoeffs2D(x, y, z, p, n=2))
--- Task4/Utility.py
+++ Task4/Utility.py
@@ -19,3 +19,30 @@ def get_xypdata(xs, xe, xstep, ps, pe, func):
     p = [random() * pcoef + ps for _ in range(len(x))]
 
     return x, y, p
+
+def get_xyzpdata(xs, xe, xstep, ys, ye, ystep,  ps, pe, func):
+    x = []
+    y = []
+    z = []
+    p = []
+    for xi in linspace(xs, xe, xstep):
+        for yi in linspace(ys, ye, ystep):
+            x.append(xi)
+            y.append(yi)
+            z.append(func(xi, yi))
+            p.append(random() * (pe - ps) + ps)
+    return x, y, z, p
+
+def read_xypzdata(filename):
+        with open(filename, "r") as f:
+            data = f.read().splitlines()
+        for i in range(len(data)):
+            data[i] = data[i].split()
+        z, y, x, p = zip(*data[1:])
+        x = list(map(float, x))
+        y = list(map(float, y))
+        z = list(map(float, z))
+        p = list(map(float, p))
+        return x, y, z, p
+
+# get_xyzpdata(-10, 10, 20, -10, 10, 20, 0, 1, lambda x, y: x + y)
